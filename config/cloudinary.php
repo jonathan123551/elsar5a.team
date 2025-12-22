498dc3070c324670cd2899f9662ae775f508e011
Jonathan <jonathan123551@gmail.com>
2025-12-23 01:46:42 +0200

    install cloudinary laravel package

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -2,14 +2,15 @@
 
 return [
 
-    'cloud_url' => env('CLOUDINARY_URL'),
+    'cloudinary' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+        'secure'     => true,
+    ],
 
-    'notification_url' => null,
-
-    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
-
-    'upload_route' => env('CLOUDINARY_UPLOAD_ROUTE'),
-
-    'upload_action' => env('CLOUDINARY_UPLOAD_ACTION'),
+    'url' => [
+        'secure' => true,
+    ],
 
 ];
