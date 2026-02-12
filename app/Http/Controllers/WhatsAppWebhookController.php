4682f0ab74a1872b5cd3522d0d3ad315c0c03671
Jonathan <jonathanmaged80@gmail.com>
2026-02-12 02:09:59 +0200

    edit blade

--- app/Http/Controllers/WhatsAppWebhookController.php
+++ app/Http/Controllers/WhatsAppWebhookController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Booking;
 
 class WhatsAppWebhookController extends Controller
 {
-   public function handle(Request $request)
-{
-    return response()->json(['ok' => true]);
-}
+    public function verify(Request $request)
+    {
+        if (
+            $request->hub_mode === 'subscribe' &&
+            $request->hub_verify_token === env('WHATSAPP_VERIFY_TOKEN')
+        ) {
+            return response($request->hub_challenge, 200);
+        }
 
+        return response('Forbidden', 403);
+    }
+
+    public function handle(Request $request)
+    {
+        return response()->json(['ok' => true]);
+    }
 }
