26b08e9120ec760eda601f38ad8d83bfbbbaef29
Jonathan <jonathanmaged80@gmail.com>
2026-03-31 01:27:28 +0200

    remove env

--- app/Http/Controllers/Admin/BookingController.php
+++ app/Http/Controllers/Admin/BookingController.php
@@ -125,7 +125,7 @@ class BookingController extends Controller
      | TEMPLATE
      ======================= */
 public function sendTicketTemplate($phone, $reference)
-{
+{ dd($reference);
     $phone = preg_replace('/[^0-9]/', '', $phone);
 
     $response = Http::withToken(env('WHATSAPP_TOKEN'))->post(
@@ -135,7 +135,7 @@ public function sendTicketTemplate($phone, $reference)
             'to' => $phone,
             'type' => 'template',
             'template' => [
-                'name' => 'ticket_final',
+                'name' => 'ticket_new',
                 'language' => [
                     'code' => 'ar_EG'
                 ],
